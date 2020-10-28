95880fc50657bc749ef88f4b22d66af2d8d116df
Issa sangare <issabmsangare@hotmail.com>
2020-10-28 16:02:57 -0400

    creates chhat and sidebar components

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sidebar.css'
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header"> 
+      </div>
+
+      <div className="sidebar__search">
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA28lsKZ9_WTGatd1N6lmZlamaxdnZsAkM",
+    authDomain: "whatsapp-clone-97618.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-97618.firebaseio.com",
+    projectId: "whatsapp-clone-97618",
+    storageBucket: "whatsapp-clone-97618.appspot.com",
+    messagingSenderId: "652246951808",
+    appId: "1:652246951808:web:daab53f6c9fe19cbff8365",
+    measurementId: "G-0GE7M7C729"
+  };
